5a3bd880180dc50dff8dc8d1531f904b9b5a56c5
Dominic Maas <dominic.maas@live.com>
2020-04-15 09:37:44 +1200

    Typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained" color="primary">
+          Connect to Camera
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
