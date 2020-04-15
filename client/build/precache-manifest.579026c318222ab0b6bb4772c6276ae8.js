ab447a0c7beaf7ed47206f82bb4710a5137bddc2
Dominic Maas <dominic.maas@live.com>
2020-04-16 11:25:18 +1200

    All build to be published (so raspberry pi does not have to build)

---
+++ client/build/precache-manifest.579026c318222ab0b6bb4772c6276ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf397d3f275e09d00e673479d46d45a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a8eb61dd9e6c1275e73",
+    "url": "/static/css/main.b31bc2e1.chunk.css"
+  },
+  {
+    "revision": "3a4a7b7db5a6053eeb3b",
+    "url": "/static/js/2.3e44c9ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3e44c9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8eb61dd9e6c1275e73",
+    "url": "/static/js/main.6c0f6838.chunk.js"
+  },
+  {
+    "revision": "9e7391fb3106933fb1ff",
+    "url": "/static/js/runtime-main.cc7335bf.js"
+  }
+]);
