78cba948a3de661f20f2e2b9486b3cd9ba08e045
Dominic Maas <dominic.maas@live.com>
2020-04-16 05:04:04 +0100

    Update Server

--- server/server.py
+++ server/server.py
@@ -1,11 +1,110 @@
 from flask import Flask
 from flask import jsonify
 from flask import Response
+from flask import request
 import gphoto2 as gp
 import time
 import io
 
+global CAMERA
+
 PORT = 8080
+CAMERA = None
+
+app = Flask(__name__)
+
+
+@app.after_request
+def after_request(response):  # Handle CORS
+    headers = response.headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
+@app.route('/set-config-item/<name>', methods=['POST'])
+def set_config_item(name):
+    # It's not connected
+    if CAMERA == None:
+        return 'The camera is not connected', 500
+
+    value = request.data.decode('UTF-8')
+
+    # Get the config
+    config = CAMERA.get_config()
+
+    # Get the widget
+    OK, widget = gp.gp_widget_get_child_by_name(config, name)
+    if OK != gp.GP_OK:
+        return 'There are no widgets with the name of ' + name, 500
+
+    OK = gp.gp_widget_set_value(widget, value)
+    if OK != gp.GP_OK:
+        return 'Could not set the value', 500
+
+    OK = gp.gp_camera_set_config(CAMERA, config)
+    if OK != gp.GP_OK:
+        return 'Could not set the config', 500
+
+    return 'Value updated successfully'
+
+
+@app.route('/get-config-item/<name>')
+def get_config_item(name):
+    # It's not connected
+    if CAMERA == None:
+        return 'The camera is not connected', 500
+
+    # Get the config
+    config = CAMERA.get_config()
+
+    # Get the widget
+    OK, widget = gp.gp_widget_get_child_by_name(config, name)
+    if OK != gp.GP_OK:
+        return 'There are no widgets with the name of ' + name, 500
+
+    choices = []
+
+    # Some config may not have choices
+    try:
+        # Get the value and the choices
+        w_val = widget.get_value()
+        choices_raw = widget.get_choices()
+
+        # Format choices
+        for c in choices_raw:
+            choices.append(c)
+    except:
+        pass
+
+    # Return formatted
+    return jsonify(
+        value=w_val,
+        choices=choices
+    )
+
+
+@app.route('/capture-preview')
+def app_capture_preview():
+    # It's not connected
+    if CAMERA == None:
+        return 'The camera is not connected', 500
+
+    # Determine the image format
+    config = gp.check_result(gp.gp_camera_get_config(CAMERA))
+    OK, image_format = gp.gp_widget_get_child_by_name(config, 'imagequality')
+    if OK >= gp.GP_OK:
+        value = gp.check_result(gp.gp_widget_get_value(image_format))
+        print('The value is ', value)
+
+    # Capture a preview image and grab the file data
+    camera_file = CAMERA.capture_preview()
+    file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
+
+    # Process the data
+    data = memoryview(file_data)
+    print(type(data), len(data))
+
+    return Response(data.tobytes(), mimetype='image/x-nikon-nef')
 
 
 def get_camera():  # Function that attempts to get the camera
@@ -25,124 +124,7 @@ def get_camera():  # Function that attempts to get the camera
         return None
 
 
-def make_response(isError, response=''):  # Wrapper around generating responses
-    return jsonify(
-        error=isError,
-        response=response,
-    )
-
-
-# ENTER PROGRAM
-app = Flask(__name__)
-
-
-@app.after_request
-def after_request(response):  # Handle CORS
-    headers = response.headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    return response
-
-
-@app.route('/set-config-item')
-def set_config_item(request):
-    # Get the camera
-    camera = get_camera()
-    if (camera == None):
-        return 'The camera is not connected', 500
-
-    # Get the config
-    config = camera.get_config()
-
-    widget = gp.gp_widget_get_child_by_name(config, 'iso')
-
-
-@app.route('/get-config-item/<name>')
-def get_config_item(name):
-    # Get the camera
-    camera = get_camera()
-    if (camera == None):
-        return 'The camera is not connected', 500
-
-    # Get the config
-    config = camera.get_config()
-
-    # Get the widget
-    OK, widget = gp.gp_widget_get_child_by_name(config, name)
-    w_val = widget.get_value()
-    w_type = widget.get_type()
-    OK, choices = gp.gp_widget_get_choices(widget)
-
-    return jsonify(
-        value=w_val,
-        choices=choices,
-        type=w_type,
-    )
-
-
-@app.route('/capture-preview')
-def app_capture_preview():
-    # Get the camera
-    camera = get_camera()
-    if (camera == None):
-        return 'The camera is not connected', 500
-
-    # Determine the image format
-    config = gp.check_result(gp.gp_camera_get_config(camera))
-    OK, image_format = gp.gp_widget_get_child_by_name(config, 'imagequality')
-    if OK >= gp.GP_OK:
-        value = gp.check_result(gp.gp_widget_get_value(image_format))
-        print('The value is ', value)
-
-    # Capture a preview image and grab the file data
-    camera_file = camera.capture_preview()
-    file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
-
-    # Process the data
-    data = memoryview(file_data)
-    print(type(data), len(data))
-
-    # Done with the camera
-    camera.exit()
-
-    return Response(data.tobytes(), mimetype='image/x-nikon-nef')
-    # return make_response(True)
-
-
-def build_config(child):
-    config = {}
-
-    label = '{}'.format(child.get_name())
-
-    if child.get_type() == gp.GP_WIDGET_SECTION:
-        res = []
-
-        for c_inner in child.get_children():
-            res.append(build_config(c_inner))
-
-        config[label] = res
-    else:
-        config[label] = '{}'.format(child.get_value())
-
-    return config
-
-
-@app.route('/get-config')
-def app_get_config():
-    # Get the camera
-    camera = get_camera()
-    if (camera == None):
-        return make_response(False, 'The camera is not connected')
-
-    config = camera.get_config()
-
-    res = []  # The response
-
-    # Build the config
-    for child in config.get_children():
-        res.append(build_config(child))
-
-    return make_response(False, res)
-
-
 # Start the web server
-app.run(host='0.0.0.0', port=PORT)
+if __name__ == '__main__':
+    CAMERA = get_camera()
+    app.run(host='0.0.0.0', port=PORT, threaded=False)
