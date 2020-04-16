82d3df3af44487ddd011bd8bff40988668f3e146
Dominic Maas <dominic.maas@live.com>
2020-04-16 16:04:10 +1200

    Update Client

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,9 @@ import { ThemeProvider } from "@material-ui/core";
 import { theme } from "./theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
   document.getElementById("root")
 );
--- client/src/pages/NightPage.tsx
+++ client/src/pages/NightPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   AppBar,
   Typography,
@@ -14,7 +14,11 @@ import {
   FormHelperText,
   TextField,
   Button,
+  Backdrop,
+  CircularProgress,
 } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import download from "downloadjs";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -22,33 +26,153 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "100%",
       marginBottom: theme.spacing(3),
     },
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: "#fff",
+    },
   })
 );
+
+class KVPair {
+  constructor(value: string, choices: string[]) {
+    this.value = value;
+    this.choices = choices;
+  }
+
+  public value: string;
+  public choices: string[];
+}
 
 function NightPage() {
   const classes = useStyles();
 
-  const [iso, setIso] = React.useState(400);
-  const [shutterSpeed, setShutterSpeed] = React.useState("1");
-  const [fStop, setFStop] = React.useState("F3.5");
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [error, setError] = React.useState<string>();
+
+  const [recShutter, setRecShutter] = React.useState<string>("?");
+
+  const [iso, setIso] = React.useState<KVPair>(new KVPair("", [""]));
+  const [shutterSpeed, setShutterSpeed] = React.useState(new KVPair("", [""]));
+  const [fStop, setFStop] = React.useState(new KVPair("", [""]));
 
   const [delay, setDelay] = React.useState("1");
   const [takes, setTakes] = React.useState("10");
 
-  // Functions
-  const takeTestPhoto = () => {
-    fetch("http://raspberrypi:8080/capture-preview")
+  useEffect(() => {
+    if (loading) return; // Prevent initial value being set
+
+    setLoading(true);
+
+    fetch("http://raspberrypi:8080/set-config-item/iso", {
+      method: "POST",
+      body: iso.value,
+    })
       .then((res) => {
-        console.log(res);
+        setLoading(false);
       })
-      .catch((reason) => {
-        alert(reason);
+      .catch((res) => {
+        setError(res);
+        setLoading(false);
       });
+  }, [iso]);
+
+  useEffect(() => {
+    if (loading) return; // Prevent initial value being set
+
+    setLoading(true);
+
+    fetch("http://raspberrypi:8080/set-config-item/shutterspeed", {
+      method: "POST",
+      body: shutterSpeed.value,
+    })
+      .then((res) => {
+        setLoading(false);
+      })
+      .catch((res) => {
+        setError(res);
+        setLoading(false);
+      });
+  }, [shutterSpeed]);
+
+  useEffect(() => {
+    if (loading) return; // Prevent initial value being set
+
+    setLoading(true);
+
+    fetch("http://raspberrypi:8080/set-config-item/f-number", {
+      method: "POST",
+      body: fStop.value,
+    })
+      .then((res) => {
+        setLoading(false);
+      })
+      .catch((res) => {
+        setError(res);
+        setLoading(false);
+      });
+  }, [fStop]);
+
+  // Set the defaults (this only runs when the component is first mounted)
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  const refresh = () => {
+    setLoading(true);
+
+    let a = fetch("http://raspberrypi:8080/get-config-item/iso")
+      .then((res) => res.json())
+      .then((res) => {
+        let iso = new KVPair(res.value, res.choices);
+        setIso(iso);
+      })
+      .catch((res) => {
+        setError("The camera is not connected, or the device is not on!");
+      });
+
+    let b = fetch("http://raspberrypi:8080/get-config-item/shutterspeed")
+      .then((res) => res.json())
+      .then((res) => {
+        let shutterSpeed = new KVPair(res.value, res.choices);
+        setShutterSpeed(shutterSpeed);
+      })
+      .catch((res) => {
+        setError("The camera is not connected, or the device is not on!");
+      });
+
+    let c = fetch("http://raspberrypi:8080/get-config-item/f-number")
+      .then((res) => res.json())
+      .then((res) => {
+        let fStop = new KVPair(res.value, res.choices);
+        setFStop(fStop);
+      })
+      .catch((res) => {
+        setError("The camera is not connected, or the device is not on!");
+      });
+
+    let d = fetch("http://raspberrypi:8080/get-config-item/focallength")
+      .then((res) => res.json())
+      .then((res) => {
+        let focal = parseFloat(res.value);
+        let exposeTime = 500 / focal;
+        setRecShutter(Math.round(exposeTime) + "");
+      })
+      .catch(() => {
+        setRecShutter("???");
+      });
+
+    // Set loading to false once complete
+    Promise.all([a, b, c, d]).then(() => {
+      setLoading(false);
+    });
   };
 
   // The UI
   return (
     <>
+      <Backdrop className={classes.backdrop} open={loading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6">Night Photography</Typography>
@@ -58,23 +182,30 @@ function NightPage() {
       <br />
 
       <Container>
+        {error ? (
+          <>
+            <Alert severity="error">{error}</Alert>
+            <br />
+          </>
+        ) : (
+          ""
+        )}
+
         <FormControl className={classes.formControl} variant="standard">
           <InputLabel id="select-iso-label">ISO</InputLabel>
           <Select
             labelId="select-iso-label"
             id="select-iso"
-            value={iso}
-            onChange={(v) => setIso(v.target.value as number)}
+            value={iso?.value}
+            onChange={(v) =>
+              setIso(new KVPair(v.target.value as string, iso.choices))
+            }
           >
-            <MenuItem value={100}>100</MenuItem>
-            <MenuItem value={200}>200</MenuItem>
-            <MenuItem value={400}>400</MenuItem>
-            <MenuItem value={800}>800</MenuItem>
-            <MenuItem value={1600}>1600</MenuItem>
-            <MenuItem value={3200}>3200</MenuItem>
-            <MenuItem value={6400}>6400</MenuItem>
-            <MenuItem value={12800}>12800</MenuItem>
-            <MenuItem value={25600}>25600</MenuItem>
+            {iso?.choices.map((v) => (
+              <MenuItem key={v} value={v}>
+                {v}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
 
@@ -83,9 +214,23 @@ function NightPage() {
           <Select
             labelId="select-shutter-speed-label"
             id="select-shutter-speed"
-            value={shutterSpeed}
-            onChange={(v) => setShutterSpeed(v.target.value as string)}
-          ></Select>
+            value={shutterSpeed.value}
+            onChange={(v) =>
+              setShutterSpeed(
+                new KVPair(v.target.value as string, shutterSpeed.choices)
+              )
+            }
+          >
+            {shutterSpeed?.choices.map((v) => (
+              <MenuItem key={v} value={v}>
+                {v}
+              </MenuItem>
+            ))}
+          </Select>
+
+          <FormHelperText>
+            Recommended shutter speed of max {recShutter}s (500 Rule)
+          </FormHelperText>
         </FormControl>
 
         <FormControl className={classes.formControl} variant="standard">
@@ -93,9 +238,18 @@ function NightPage() {
           <Select
             labelId="select-f-stop-label"
             id="select-f-stop"
-            value={fStop}
-            onChange={(v) => setFStop(v.target.value as string)}
-          ></Select>
+            value={fStop.value}
+            onChange={(v) =>
+              setFStop(new KVPair(v.target.value as string, fStop.choices))
+            }
+          >
+            {" "}
+            {fStop?.choices.map((v) => (
+              <MenuItem key={v} value={v}>
+                {v}
+              </MenuItem>
+            ))}
+          </Select>
         </FormControl>
 
         <FormControl className={classes.formControl} variant="standard">
@@ -125,9 +279,12 @@ function NightPage() {
         </FormControl>
 
         <FormControl className={classes.formControl}>
-          <Button variant="outlined" onClick={takeTestPhoto}>
-            Take test photo
+          <Button variant="outlined" onClick={refresh}>
+            Refresh
           </Button>
+          <FormHelperText>
+            Refresh after adjusting the camera focus or zoom
+          </FormHelperText>
         </FormControl>
       </Container>
     </>
